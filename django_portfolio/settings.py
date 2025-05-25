2a8afb0c11051373c668ca2fd11cc0d323dedde3
Miguel Ojeda <cabrerasm081@gmail.com>
2025-05-25 10:30:33 -0400

    .

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -16,7 +16,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-print(os.getenv('DATABASE_URL'))
 DATABASES = {
     # 'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     'default': {
